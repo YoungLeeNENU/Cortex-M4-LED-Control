5655d3c0f1840a78319e8c2d3c4b3e170a3d58bf
李暘 <YoungLeeNENU@gmail.com>
2012-08-28 20:48:58 +0800

    未完成

--- JavaClient/Board_Server.c
+++ JavaClient/Board_Server.c
@@ -9,10 +9,10 @@
 char char_led;    /* 接收到的客户端的LED编号 */
 int  int_led;    /* 服务端的LED编号 */
 
-static int flag1 = 0;    /* LED1的标志位 */
-static int flag2 = 0;    /* LED2的标志位 */
-static int flag3 = 0;    /* LED3的标志位 */
-static int flag4 = 0;    /* LED4的标志位 */
+static int led_flag1 = 0;    /* LED1的标志位 */
+static int led_flag2 = 0;    /* LED2的标志位 */
+static int led_flag3 = 0;    /* LED3的标志位 */
+static int led_flag4 = 0;    /* LED4的标志位 */
 
 /* 对LED进行操作 */
 void action( int led_num );
@@ -40,9 +40,9 @@ void action( int led_num )
 	 /* LED编号为1 */
 	 if (1 == led_num)
 	 {
-		  flag1++;
+		  led_flag1++;
 		  
-		  if (0 == flag1 % 2)
+		  if (0 == led_flag1 % 2)
 			   printf ("LED1 OFF...\n");
 		  else
 			   printf ("LED1 ON...\n");
@@ -50,9 +50,9 @@ void action( int led_num )
 	 /* LED编号为2 */
 	 else if (2 == led_num)
 	 {
-		  flag2++;
+		  led_flag2++;
 		  
-		  if (0 == flag2 % 2)
+		  if (0 == led_flag2 % 2)
 			   printf ("LED2 OFF...\n");
 		  else
 			   printf ("LED2 ON...\n");
@@ -60,9 +60,9 @@ void action( int led_num )
 	 /* LED编号为3 */
 	 else if (3 == led_num)
 	 {
-		  flag3++;
+		  led_flag3++;
 		  
-		  if (0 == flag3 % 2)
+		  if (0 == led_flag3 % 2)
 			   printf ("LED3 OFF...\n");
 		  else
 			   printf ("LED3 ON...\n");
@@ -70,9 +70,9 @@ void action( int led_num )
 	 /* LED编号为4 */
 	 else if (4 == led_num)
 	 {
-		  flag4++;
+		  led_flag4++;
 		  
-		  if (0 == flag4 % 2)
+		  if (0 == led_flag4 % 2)
 			   printf ("LED4 OFF...\n");
 		  else
 			   printf ("LED4 ON...\n");
