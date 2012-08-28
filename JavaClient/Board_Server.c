687614c7a5ffa254397bbe03c0eaf5bf40fbde6d
李暘 <YoungLeeNENU@gmail.com>
2012-08-28 22:28:50 +0800

    未完成

--- JavaClient/Board_Server.c
+++ JavaClient/Board_Server.c
@@ -6,19 +6,30 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define LED_ON   1
+#define LED_OFF  0
+#define LED1     1
+#define LED2     2
+#define LED3     3
+#define LED4     4
+#define STOP_LED 0
+
 char char_led;    /* 接收到的客户端的LED编号 */
 int  int_led;    /* 服务端的LED编号 */
 
-static int led_flag1 = 0;    /* LED1的标志位 */
-static int led_flag2 = 0;    /* LED2的标志位 */
-static int led_flag3 = 0;    /* LED3的标志位 */
-static int led_flag4 = 0;    /* LED4的标志位 */
+static int led_flag  = LED_ON;    /* 停止标志位 */
+static int led_flag1 = LED_ON;    /* LED1的标志位 */
+static int led_flag2 = LED_ON;    /* LED2的标志位 */
+static int led_flag3 = LED_ON;    /* LED3的标志位 */
+static int led_flag4 = LED_ON;    /* LED4的标志位 */
 
 /* 对LED进行操作 */
 void action( int led_num );
 
 int main(int argc, char *argv[])
 {
+	 /* int MyStreamSocket = socket( int domain, int type, int protocol ); */
+	 
 	 do
 	 {
 		  /* 接收从java客户端发来的LED灯的编号 */
@@ -29,7 +40,7 @@ int main(int argc, char *argv[])
 
 		  /* 根据java客户端LED灯的编号对LED进行操作 */
 		  action( int_led );
-	 } while (1);
+	 } while (LED_ON == led_flag);
 	 
 	 return 0;
 }
@@ -37,45 +48,48 @@ int main(int argc, char *argv[])
 /* 对LED进行操作 */
 void action( int led_num )
 {
+	 /* 停止位为0 */
+	 if (STOP_LED == led_num)
+		  led_flag = LED_OFF;
 	 /* LED编号为1 */
-	 if (1 == led_num)
+	 else if (LED1 == led_num)
 	 {
-		  led_flag1++;
-		  
-		  if (0 == led_flag1 % 2)
+		  if (LED_OFF == led_flag1 % 2)
 			   printf ("LED1 OFF...\n");
 		  else
 			   printf ("LED1 ON...\n");
+
+		  led_flag1++;
 	 }
 	 /* LED编号为2 */
-	 else if (2 == led_num)
+	 else if (LED2 == led_num)
 	 {
-		  led_flag2++;
-		  
-		  if (0 == led_flag2 % 2)
+		  if (LED_OFF == led_flag2 % 2)
 			   printf ("LED2 OFF...\n");
 		  else
 			   printf ("LED2 ON...\n");
+
+		  led_flag2++;
 	 }
 	 /* LED编号为3 */
-	 else if (3 == led_num)
+	 else if (LED3 == led_num)
 	 {
-		  led_flag3++;
-		  
-		  if (0 == led_flag3 % 2)
+		  if (LED_OFF == led_flag3 % 2)
 			   printf ("LED3 OFF...\n");
 		  else
 			   printf ("LED3 ON...\n");
+
+		  led_flag3++;
 	 }
 	 /* LED编号为4 */
-	 else if (4 == led_num)
+	 else if (LED4 == led_num)
 	 {
-		  led_flag4++;
-		  
-		  if (0 == led_flag4 % 2)
+		  if (LED_OFF == led_flag4 % 2)
 			   printf ("LED4 OFF...\n");
 		  else
 			   printf ("LED4 ON...\n");
+
+		  led_flag4++;
 	 }
 	 /* 错误LED编号 */
 	 else
