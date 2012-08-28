a0fc9c64caef1613b80b7e713ddd6fa872ecd883
李暘 <YoungLeeNENU@gmail.com>
2012-08-28 19:50:04 +0800

    服务端和客户端

--- JavaClient/PC_Server.c
+++ JavaClient/PC_Server.c
@@ -1,8 +1,69 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+char char_led;
+int int_led;
+
+int flag1 = 0;
+int flag2 = 0;
+int flag3 = 0;
+int flag4 = 0;
+
+void action( int led_num );
+
 int main(int argc, char *argv[])
 {
+	 do
+	 {
+		  scanf( "%c", &char_led );
+		  getchar(  );
+
+		  int_led = char_led - '0';
+
+		  action( int_led );
+	 } while (1);
 	 
 	 return 0;
+}
+
+void action( int led_num )
+{
+	 if (1 == led_num)
+	 {
+		  flag1++;
+		  if (0 == flag1 % 2)
+			   printf ("LED1 OFF...\n");
+		  else
+			   printf ("LED1 ON...\n");
+	 }
+	 else if (2 == led_num)
+	 {
+		  flag2++;
+		  if (0 == flag2 % 2)
+			   printf ("LED2 OFF...\n");
+		  else
+			   printf ("LED2 ON...\n");
+	 }
+	 else if (3 == led_num)
+	 {
+		  flag3++;
+		  if (0 == flag3 % 2)
+			   printf ("LED3 OFF...\n");
+		  else
+			   printf ("LED3 ON...\n");
+	 }
+	 else if (4 == led_num)
+	 {
+		  flag4++;
+		  if (0 == flag4 % 2)
+			   printf ("LED4 OFF...\n");
+		  else
+			   printf ("LED4 ON...\n");
+	 }
+	 else
+	 {
+		  perror( "LED NUMBER ERROR...\n" );
+		  exit( 0 );
+	 }
 }
